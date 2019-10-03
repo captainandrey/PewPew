b922a74a3247dcb70b1a5339cb23665cd64de043
captainandrey <43338827+captainandrey@users.noreply.github.com>
2019-10-03 13:39:38 +0200

    Initial commit

---
+++ Assets/Scripts/CameraController3p.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController3p : MonoBehaviour
+{
+
+    public float RotationSpeed = 1;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+    const string mouseXName = "Mouse X";
+    const string mouseYName = "Mouse Y";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        MoveCamera();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveCamera()
+    {
+        mouseX += Input.GetAxis(mouseXName) * RotationSpeed;
+        mouseY -= Input.GetAxis(mouseYName) * RotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+
+    }
+}
---
+++ Assets/Scripts/CharacterController3p.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController3p : MonoBehaviour
+{
+    public float Speed = 5;
+    public const string HorizontalAxisName = "Horizontal";
+    public const string VerticalAxisName = "Vertical";
+    Rigidbody rb;
+    public float JumpForce = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+    void FixedUpdate()
+    {
+        
+    }
+    void MovePlayer()
+    {
+        var horizontalInput = Input.GetAxis(HorizontalAxisName);
+        var verticalInput = Input.GetAxis(VerticalAxisName);
+        var movement = new Vector3(horizontalInput, 0f, verticalInput).normalized * Speed * Time.deltaTime;
+
+        var y = rb.velocity.y;
+        if (Input.GetButtonDown("Jump"))
+        {
+            y = JumpForce;
+        }
+
+
+        rb.velocity = new Vector3(0, y, 0);
+        transform.Translate(movement, Space.Self);
+
+    }
+}
